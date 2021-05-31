1f16cce7004acf28a55e16dca6598e4791d6e478
mdimran1409036 <mdimran1409036@gmail.com>
2021-05-31 07:37:12 +0600

    Admin adding confirmation on record deletion (category and product)

--- resources/views/livewire/admin/admin-product-component.blade.php
+++ resources/views/livewire/admin/admin-product-component.blade.php
@@ -55,7 +55,8 @@
                                         <td>{{ $product->created_at->diffForHumans() }}</td>
                                         <td>
                                             <a href="{{ route('admin.editproduct',['product_slug'=>$product->slug]) }}" title="Edit"><i class="fa fa-edit fa-2x"></i> </a>
-                                            <a href="#" wire:click.prevent='deleteProduct({{ $product->id }})'  
+                                            <a href="#" onclick="confirm('are you sure want to delete this?')|| event.stopImmediatePropagation()"
+                                                wire:click.prevent='deleteProduct({{ $product->id }})'  
                                                 style="margin-left: 15px" title="Delete"> 
                                                 <i class="fa fa-times text-danger  fa-2x"></i> 
                                             </a>
